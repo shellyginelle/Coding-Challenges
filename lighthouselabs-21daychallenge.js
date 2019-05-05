becbfd7e99abed562eac164ff9d095b4a2a1e3ed
Shelly Sicat <shellygsicat@gmail.com>
2019-05-04 18:14:57 -0600

    Add challenge 4

--- lighthouselabs-21daychallenge.js
+++ lighthouselabs-21daychallenge.js
@@ -1,62 +1,57 @@
-//Challenge 1
+//Challenge 1 - function powerOn()
 function powerOn() {
-  ship.powerOn = true;
+    ship.powerOn = true;
 }
 
-//Challenge 2
+//Challenge 2 - function countModules()
 function countModules() {
-  return availableModules.length;
+    return availableModules.length;
 }
 
-//Challenge 3
+//Challenge 3 - function countEssential()
 function countEssential() {
-  var countEssential = 0;
-  for (let i=0; i<availableModules.length; i++){
-    if (availableModules[i].essential === true){
-      countEssential += 1;
-    }
-    else {
-      //do nothing
-    }
-  }
-  return countEssential;
-}
-
-//Challenge 4
-function setEnabled() {
-  for (let i=0; i<availableModules.length; i++){
-    if (availableModules[i].enabled === false){
-      availableModules[i].enabled = true;
-    }
-    else {
-      //do nothing
-    }
-  }
-}
-
-// TODO: You need to loop over the availableModules and find the module called "life-support" and get its index, then use it to call loadModule().
-function isLifeSupport() {
+    var countEssential = 0;
     for (let i=0; i<availableModules.length; i++){
-      if (availableModules[i].name === 'life-support'){
-        return availableModules[i];
+        if (availableModules[i].essential === true){
+            countEssential += 1;
+        }
+        else {
+            //do nothing
+        }
+    }
+    return countEssential;
+}
+
+//Challenge 4 - functions loadModule(index), getToLoadModules(), isLifeSupport()
+function loadModule(index) {
+    ship.modules.push(availableModules[index]);
+}
+
+function getToLoadModules() {
+    for (let i=0; i<availableModules.length; i++){
+      if (availableModules[i].enabled === false){
+        availableModules[i].enabled = true;
+        loadModule(i);
       }
       else {
-        //do nothing
+        loadModule(i);
       }
     }
-  }
-
-function loadModule(index) {
-  setEnabled(); //run to set object's properties to true
-  
-  //load three essential modules ONLY?
-  for (let i=0; i<availableModules.length; i++){
-    if (availableModules[i].essential === true){
-      modules[i].push;
-    }
-    else {
-      //do nothing
-    }
-  }
-
 }
+
+function isLifeSupport() {
+    var index = 0;
+    for (let i=0; i<availableModules.length; i++){
+        if (availableModules[i].name === 'life-support'){
+            index = i;
+            return index;
+        }
+        else {
+            //do nothing
+        }
+    }
+    loadModule(index);
+}
+
+getToLoadModules();
+isLifeSupport();
